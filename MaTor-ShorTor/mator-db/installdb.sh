1c66553ef2d3aab74d4e56b4d8c8613818e39e31
Sacha Servan-Schreiber <8027773+sachaservan@users.noreply.github.com>
2024-12-06 14:35:09 -0500

    Fix db install script

--- MaTor-ShorTor/mator-db/installdb.sh
+++ MaTor-ShorTor/mator-db/installdb.sh
@@ -9,9 +9,9 @@ mkdir -p go/src/mator-db
 cp src/*.go go/src/mator-db/
 cp -r src/asn go/src/mator-db/
 
-#export GOPATH=`pwd`/go
-export GOPATH="$SCRIPTPATH/go"
 cd go/src/mator-db/
+go mod init mator-db
 go mod tidy
+go build -o ../../bin/mator-db
 cd -
 cp go/bin/mator-db ./
